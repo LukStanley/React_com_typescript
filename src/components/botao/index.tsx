9f3cd5c5cede6f654cc6bec283c0b4619317b245
LukStanley <lukksterror.com>
2022-12-12 21:49:50 -0300

    Aprendendo a criar componentes para usar em nossa aplicação

---
+++ src/components/botao/index.tsx
@@ -0,0 +1,18 @@
+
+/* É deste jeito que importamos usando react, criamos a aplicação 
+e importamos para nosso html principal*/
+import React from "react";
+
+class Botao extends React.Component{
+
+    render(){
+        return(
+            <button>
+                Botão
+            </button>
+
+        )
+    }
+}
+
+export default Botao;
