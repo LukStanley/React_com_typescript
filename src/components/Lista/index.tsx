e16fa0c08e37ec1d1223b7cfcd29aa81924dc843
LukStanley <lukksterror.com>
2022-12-12 23:12:03 -0300

    Criando nossa lista de estudos usando uma function component

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,34 @@
+//É desta maneira que escrevemos uma component Function em React
+
+import React from 'react'
+
+function Lista(){
+    return(
+        <aside>
+            <h2> Estudos do dia</h2>    
+                <ul>    
+                    <li>
+                        <h3>
+                            React
+                        </h3>    
+                        <span>
+                         02:00:00
+                        </span>
+                       
+                    </li>
+                    <li>
+                         <h3>
+                            Javascript
+                         </h3>   
+                         <span>
+                            01:00:00
+                         </span>
+                       
+                    </li>
+
+                </ul>
+        </aside>
+    )}
+
+
+export default Lista;
